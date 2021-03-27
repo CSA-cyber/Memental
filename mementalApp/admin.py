fd8daae81aef431c69c5590cea87e06563380c4f
CSA-cyber <shafi.ahmed@g.bracu.ac.bd>
2021-03-27 23:01:47 +0600

    created db

--- mementalApp/admin.py
+++ mementalApp/admin.py
@@ -1,8 +1,9 @@
-# from django.contrib import admin
-# from .models import *
-# from django.forms import CheckboxSelectMultiple
+from django.contrib import admin
+from .models import *
+from django.forms import CheckboxSelectMultiple
 
 
-# admin.site.register(User)
-# admin.site.register(Appointment)
-# admin.site.register(UserMessage)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(UserMessage)
