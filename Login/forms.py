abed37c77931b9e2204d2528e7d297e01f9bda28
Shafi Ahmed <shafi.ahmed@g.bracu.a.bd>
2021-04-25 16:53:39 +0600

    Doctor login functional
    * Updated styles in doctor signup page
    * Updated styles in login page

--- Login/forms.py
+++ Login/forms.py
@@ -24,22 +24,69 @@ class patient_create_form(forms.Form):
 
     bdate = forms.DateField(
         label="Birthdate", input_formats=settings.DATE_INPUT_FORMATS, widget=forms.SelectDateWidget(attrs={'class': 'form-date'}, years=[*range(date.today().year)][::-1]))
-    credit_card = forms.CharField(label="Credit Card", widget=forms.TextInput({'name':"additional", 'class':"additional", 'id':"additional", 'placeholder': "Credit card"}), required=not True)
-    street_address = forms.CharField(widget=forms.TextInput({'name':"street", 'class':"street", 'id':"street", 'placeholder': "House + Street Number"}))
-    zip_code = forms.IntegerField(widget=forms.TextInput({'name':"zip", 'class':"zip", 'id':"zip", 'placeholder':"Zip Code"}))
+    credit_card = forms.CharField(label="Credit Card", widget=forms.TextInput(
+        {'name': "additional", 'class': "additional", 'id': "additional", 'placeholder': "Credit card"}), required=not True)
+    street_address = forms.CharField(widget=forms.TextInput(
+        {'name': "street", 'class': "street", 'id': "street", 'placeholder': "House + Street Number"}))
+    zip_code = forms.IntegerField(widget=forms.TextInput(
+        {'name': "zip", 'class': "zip", 'id': "zip", 'placeholder': "Zip Code"}))
     district = forms.ChoiceField(choices=[('', 'District'),
-                                          ('Dhaka','Dhaka'),
+                                          ('Dhaka', 'Dhaka'),
                                           ('Rajsahi', 'Rajsahi'),
-                                          ('Chittagong','Chittagong'),
+                                          ('Chittagong', 'Chittagong'),
                                           ('Barishal', 'Barishal'),
                                           ('Sylhet', 'Sylhet')
-                                         ])
+                                          ])
     phone_type = forms.ChoiceField(choices=[('', 'Phone'),
-                                            ('01', '01'), 
+                                            ('01', '01'),
                                             ('02', '02')
-                                           ])
+                                            ])
     phone = forms.CharField(label="Phone no", widget=forms.TextInput(attrs={
                             'name': "phone", 'class': 'phone', 'id': "phone", 'placeholder': "Phone Number"}))
+
+
+class doctor_create_form(forms.Form):
+    fname = forms.CharField(label='First Name', widget=forms.TextInput(attrs={
+                            'name': "first_name", 'id': "first_name", 'class': "input-text", 'placeholder': "First Name"}))
+    lname = forms.CharField(label='Last Name', widget=forms.TextInput(attrs={
+                            'name': "last_name", 'id': "last_name", 'class': "input-text", 'placeholder': "Last Name"}))
+
+    email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={
+                             'name': "company", 'class': "company", 'id': "company", 'placeholder': "Email"}))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(
+        attrs={'name': "company", 'class': "company", 'id': "company", 'placeholder': 'Password'}))
+    password2 = forms.CharField(label="Password", widget=forms.PasswordInput(
+        attrs={'name': "company", 'class': "company", 'id': "company", 'placeholder': 'Re-Enter Password'}))
+
+    bdate = forms.DateField(
+        label="Birthdate", input_formats=settings.DATE_INPUT_FORMATS, widget=forms.SelectDateWidget(attrs={'class': 'form-date'}, years=[*range(date.today().year)][::-1]))
+
+    qualifications = forms.CharField(label='Qualifications', widget=forms.Textarea(
+        {'name': "qualifications", 'class': "qualifications", 'id': "qualifications", 'placeholder': "List of Qualifications"}))
+    specilization = forms.CharField(
+        widget=forms.TextInput({'placeholder': "Specialization"}))
+    credit_card = forms.CharField(label="Credit Card", widget=forms.TextInput(
+        {'name': "additional", 'class': "additional", 'id': "additional", 'placeholder': "Credit card"}), required=not True)
+    street_address = forms.CharField(widget=forms.TextInput(
+        {'name': "street", 'class': "street", 'id': "street", 'placeholder': "House + Street Number"}))
+    zip_code = forms.IntegerField(widget=forms.TextInput(
+        {'name': "zip", 'class': "zip", 'id': "zip", 'placeholder': "Zip Code"}))
+    district = forms.ChoiceField(choices=[('', 'District'),
+                                          ('Dhaka', 'Dhaka'),
+                                          ('Rajsahi', 'Rajsahi'),
+                                          ('Chittagong', 'Chittagong'),
+                                          ('Barishal', 'Barishal'),
+                                          ('Sylhet', 'Sylhet')
+                                          ])
+    phone_type = forms.ChoiceField(choices=[('', 'Phone'),
+                                            ('01', '01'),
+                                            ('02', '02')
+                                            ])
+    phone = forms.CharField(label="Phone no", widget=forms.TextInput(attrs={
+                            'name': "phone", 'class': 'phone', 'id': "phone", 'placeholder': "Phone Number"}))
+
+    education = forms.CharField(label='Education', widget=forms.Textarea(
+        {'name': "education", 'class': "education", 'id': "education", 'placeholder': "List of Med-schools"}))
 
 
 class login_form(forms.Form):
--- Login/views.py
+++ Login/views.py
@@ -1,4 +1,4 @@
-from Hospital.models import Patient
+from Hospital.models import Doctor, Patient
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
@@ -13,6 +13,10 @@ from .forms import *
 def login_user(request):
     form = login_form()
     context = {}
+    if request.user.is_authenticated:
+        messages.info(request, 'You are alerady logged in.')
+        return redirect('index')
+    
     if request.method == 'POST':
         form = login_form(request.POST)
         if form.is_valid():
@@ -20,12 +24,20 @@ def login_user(request):
             password = form.cleaned_data['password']
             user = authenticate(request, username=email, password=password)
             if user:
+                doctor = Doctor.objects.get(user=user)
+                if doctor:
+                    if not doctor.verified:
+                        messages.warning(
+                            request, "Need adminstration approval to continue.")
+                        return redirect('login')
+
                 login(request, user)
                 if user.is_superuser:
                     return HttpResponseRedirect(reverse('admin:index'))
+
                 return redirect('index')
             else:
-                messages.info(request, 'Email and password did not match')
+                messages.info(request, 'Email and password do not match')
                 return redirect('login')
 
     context = {'form': form}
@@ -33,6 +45,10 @@ def login_user(request):
 
 
 def logout_user(request):
+    if not request.user.is_authenticated:
+        messages.info(request, 'You are not logged in.')
+        return redirect('index')
+    
     logout(request)
     messages.success(request, 'user logged out')
     return render(request, 'index.html')
@@ -43,6 +59,54 @@ def create_account(request):
 
     if request.method == 'POST':
         form = patient_create_form(request.POST)
+
+        if form.is_valid():
+            fname = form.cleaned_data['fname']
+            lname = form.cleaned_data['lname']
+            name = f'{fname} {lname}'
+
+            email = form.cleaned_data['email']
+            if Patient.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('login')
+
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            
+            if password1 != password2:
+                messages.warning(request, "Passwords don't match")
+                return redirect('signup')
+            
+            bdate = form.cleaned_data['bdate']
+            credit_card = form.cleaned_data['credit_card']
+
+            street_address = form.cleaned_data['street_address']
+            zip_code = form.cleaned_data['zip_code']
+            district = form.cleaned_data['district']
+
+            address = f'{street_address}, {district}-{zip_code}'
+
+            phone = form.cleaned_data['phone']
+            phone_type = form.cleaned_data['phone_type']
+            phone = f'{phone_type}{phone}'
+            user = User.objects.create_user(email, email, password1)
+
+            Patient.objects.create(user=user, name=name, email=email, password=password1,
+                                   birthdate=bdate, credit_info=credit_card, phone=phone, address=address)
+
+            messages.success(request, 'Account created')
+
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
+
+
+def doctor_signup(request):
+    form = doctor_create_form()
+
+    if request.method == 'POST':
+        form = doctor_create_form(request.POST)
 
         if form.is_valid():
             print('valid form')
@@ -58,6 +122,8 @@ def create_account(request):
             password1 = form.cleaned_data['password1']
             password2 = form.cleaned_data['password2']
             bdate = form.cleaned_data['bdate']
+            qualifications = form.cleaned_data['qualifications']
+
             credit_card = form.cleaned_data['credit_card']
 
             street_address = form.cleaned_data['street_address']
@@ -69,17 +135,15 @@ def create_account(request):
             phone = form.cleaned_data['phone']
             phone_type = form.cleaned_data['phone_type']
             phone = f'{phone_type}{phone}'
+            education = form.cleaned_data['education']
+            specilization = form.cleaned_data['specilization']
             user = User.objects.create_user(email, email, password1)
             # user.save()
-            patient = Patient.objects.create(
-                user=user, name=name, email=email, password=password1, birthdate=bdate,
-                credit_info=credit_card, phone=phone, address=address)
-            # patient.save()
+            Doctor.objects.create(user=user, name=name, email=email, password=password1, birthdate=bdate, credit_info=credit_card,
+                                  phone=phone, address=address, qualifications=qualifications, specilization=specilization, education=education)
+            messages.success(request, 'Account created. Wait for approval.')
+
             return redirect('login')
 
     context = {'form': form}
-    return render(request, 'signup.html', context)
-
-
-def doctor_signup(request):
-    return render(request, 'doctor_signup.html')
+    return render(request, 'doctor_signup.html', context)
