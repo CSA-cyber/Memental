3766cf03713b1c1798d3db897fe4c3c690877ce3
Aditya Roy <54760764+CodenameCypher@users.noreply.github.com>
2021-04-28 12:33:31 +0600

    pdf generation and downloading up and workin

--- Hospital/views.py
+++ Hospital/views.py
@@ -4,7 +4,12 @@ from datetime import datetime
 from .forms import *
 from django.template import RequestContext
 from Payment.models import Appointment
-
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from Memental import settings
+from django.http import HttpResponse
+from reportlab.lib.units import cm
 # Create your views here.
 
 
@@ -69,7 +74,7 @@ def show_profile(request):
             doctor = Doctor.objects.get(user=user)
             form = UpdateDoctorForm()
             approve_form = ApproveForm()
-            
+
             form.fields['phone'].widget.attrs['value'] = doctor.phone
             form.fields['credit_card'].widget.attrs['value'] = doctor.credit_info
             form.fields['address'].widget.attrs['value'] = doctor.address
@@ -80,10 +85,8 @@ def show_profile(request):
             form.fields['availability'].initial = doctor.availability
             form.fields['specilization'].initial = doctor.specilization
             appointments = Appointment.objects.filter(doctor=doctor)
-            
+
             approve_form.fields['approval'].initial = doctor.specilization
-            
-            
 
             if request.method == 'POST':
                 form = UpdateDoctorForm(request.POST)
@@ -110,7 +113,7 @@ def show_profile(request):
                     doctor.availability = form.cleaned_data['availability']
                     doctor.specilization = form.cleaned_data['specilization']
                     doctor.save()
-                    
+
                 elif approve_form.is_valid():
                     approval = approve_form.cleaned_data['approval']
                     approval = int(approval)
@@ -121,7 +124,7 @@ def show_profile(request):
                     appointment.approval = approval
                     appointment.save()
 
-            return render(request, 'profile_doctor.html', {'doctor': doctor, 'appointments': appointments, 'form': form, 'approve_form':approve_form})
+            return render(request, 'profile_doctor.html', {'doctor': doctor, 'appointments': appointments, 'form': form, 'approve_form': approve_form})
     else:
         return redirect('login')
 
@@ -129,3 +132,89 @@ def show_profile(request):
 def show_emergency(request):
     cabins = Emergency_Cabin.objects.all()
     return render(request, 'emergency.html', {'cabins': cabins})
+
+
+def generate_prescription(request, pk):
+    appointments = Appointment.objects.get(id=pk)
+    patient = appointments.patient
+    doctor = appointments.doctor
+
+    patient_name = "Name: " + patient.name
+    patient_email = "Email: " + patient.email
+    patient_birthdate = "Birthdate: " + str(patient.birthdate)
+    patient_phone = "Phone: " + patient.phone
+    patient_address = "Address: " + patient.address
+    disease_details = "Disease: " + appointments.disease_details
+    appointment_date = "Appointment Date: " + appointments.date.strftime("%A") + ", " + appointments.date.strftime(
+        "%d")+"/" + appointments.date.strftime("%m") + "/" + appointments.date.strftime("%Y")
+
+    pdfName = patient.name + " Prescription_" + pk + '.pdf'
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename='+pdfName
+    pdf = canvas.Canvas(response)
+    pdf.setTitle(appointments.patient.get_fname)
+
+    pdfmetrics.registerFont(
+        TTFont('bangers', 'Hospital/pdf_asset/Bangers-Regular.ttf')
+    )
+    time = datetime.now()
+    time1 = "Date: "+time.strftime("%A") + ", " + time.strftime("%d")+"/" + time.strftime("%m") + \
+        "/" + time.strftime("%Y")
+    time2 = "Time: "+time.strftime("%I")+":" + time.strftime("%M") + \
+        ":" + time.strftime("%S") + " " + time.strftime("%p")
+    pdf.drawInlineImage('Hospital/pdf_asset/mental-icon-2.jpg',
+                        60, 750, width=40, height=40)
+    pdf.drawInlineImage('Hospital/pdf_asset/steth.png',
+                        400, 50, width=150, height=120)
+    pdf.setFont('bangers', 22)
+    pdf.drawString(105, 760, "Memental")
+    pdf.setFont('Courier', 16)
+    pdf.drawString(188, 763, "Health Care")
+    pdf.setFont('Courier', 12)
+    pdf.drawString(55, 730, time1)
+    pdf.drawString(55, 717, time2)
+    pdf.line(50, 50, 50, 800)
+    pdf.line(50, 50, 550, 50)
+    pdf.line(50, 800, 550, 800)
+    pdf.line(550, 50, 550, 800)
+    pdf.line(50, 708, 550, 708)
+    pdf.line(50, 587, 550, 587)
+
+    pdf.drawString(55, 690, 'Patient Details:')
+    pdf.drawString(55, 690, 'Patient Details:')
+    pdf.drawString(55, 690, 'Patient Details:')
+    pdf.setFont('Courier', 12)
+    pdf.drawString(380, 777, "Dr. "+doctor.name.split()[-1])
+    pdf.drawString(380, 777, "Dr. "+doctor.name.split()[-1])
+    pdf.drawString(380, 777, "Dr. "+doctor.name.split()[-1])
+    pdf.drawString(380, 762, doctor.specilization)
+    pdf.drawString(380, 747, "Phone: "+doctor.phone)
+    pdf.drawString(100, 675, patient_name)
+    pdf.drawString(100, 675, patient_name)
+    pdf.drawString(100, 675, patient_name)
+    pdf.drawString(100, 661, patient_birthdate)
+    pdf.drawString(100, 648, patient_email)
+    pdf.drawString(100, 635, patient_phone)
+    pdf.drawString(100, 622, patient_address)
+    pdf.drawString(100, 609, disease_details)
+    pdf.drawString(100, 609, disease_details)
+    pdf.drawString(100, 609, disease_details)
+    pdf.drawString(100, 596, appointment_date)
+    pdf.drawString(100, 596, appointment_date)
+    pdf.drawString(100, 596, appointment_date)
+    pdf.drawString(55, 570, 'Precription:')
+    pdf.drawString(55, 570, 'Precription:')
+    pdf.drawString(55, 570, 'Precription:')
+
+    textobject = pdf.beginText(100, 550)
+    i = 1
+    for line in appointments.prescription.splitlines(False):
+        textobject.textLine(str(i) + ") " + line.rstrip())
+        i = i + 1
+    pdf.drawText(textobject)
+
+    pdf.save()
+    i = 1
+
+    return response
