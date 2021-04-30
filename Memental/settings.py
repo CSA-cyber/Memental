ccc27b6833fba36493dc95ffa5e5e0572e45bccf
Shafi Ahmed <shafi.ahmed@g.bracu.a.bd>
2021-05-01 00:29:53 +0600

    Added MVT strructure folder

--- Memental/settings.py
+++ Memental/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/'
 ]
 
-MEDIA_URL = 'images/'
+MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR/'static/images/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
